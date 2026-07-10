fe4a1710599bb69fdc6db81976b73bd158ebecdc
jarengurr <jarenwheat@gmail.com>
2026-07-09 17:09:31 -0700

    update

--- scripts/date.js
+++ scripts/date.js
@@ -4,6 +4,6 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 //alert(document.lastModified);
 // returns: Tuesday, December 16, 2017 11:09:42
 //let oLastModif = new Date(document.lastModified)
-
+// sets date
 let lmDate = document.lastModified
 document.getElementById("lastModified").innerHTML = lmDate;
